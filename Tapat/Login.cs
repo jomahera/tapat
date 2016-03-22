358ac94ba9ee5d28816f57a6466801f504d17231
Joanna Heramil <joannaheramil@outlook.com>
2016-03-22 22:53:08 +0800

    First ever iteration

---
+++ Tapat/Login.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Tapat
+{
+    [Activity(Label = "Tapat", MainLauncher = true, Icon = "@drawable/icon")]
+    public class Login : Activity
+    {
+     
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Login);
+
+            Button login = FindViewById<Button>(Resource.Id.login_button);
+            EditText user = FindViewById<EditText>(Resource.Id.user_id);
+            EditText pass = FindViewById<EditText>(Resource.Id.password);
+
+            //Login functionality
+            login.Click += delegate
+            {
+                if (user.Text != null)
+                {
+                    StartActivity(typeof(Tapat.Fingerprint_BEI));
+                }
+            };
+        }
+    }
+}
+
---
+++ Tapat/fingerprint_bei.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Tapat
+{
+    [Activity(Label = "Fingerprint_BEI")]
+    public class Fingerprint_BEI : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.Fingerprint_BEI);
+
+            ImageButton fp = FindViewById<ImageButton>(Resource.Id.img_fp);
+
+            fp.Click += delegate { StartActivity(typeof(Tapat.MainMenu)); };
+        }
+    }
+}
